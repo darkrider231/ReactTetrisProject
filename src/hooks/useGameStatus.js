be18263d6e15698dd97cbf59b2d2733572cde74b
Grant Patterson <darkrider231@gmail.com>
2021-04-27 15:14:35 -0400

    Partically Completed React-Tetris Game for Class Final Project

---
+++ src/hooks/useGameStatus.js
@@ -0,0 +1,23 @@
+import { useStage, useEffect, useCallback, useState } from 'react';
+
+export const useGameStatus = rowsCleared => {
+    const [score, setScore] = useState(0);
+    const [rows, setRows] = useState(0);
+    const [level, setLevel] = useState(0);
+
+    const linePoints = [40, 100, 300, 1200]; // original score from Tetris
+
+    const calcScore = useCallback(() => {
+        // We have score
+        if (rowsCleared > 0) {
+            // This is how original Tetris score is calculated
+            setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1));
+            setRows(prev => prev + rowsCleared);
+        }
+    }, [level, linePoints, rowsCleared]);
+
+    useEffect(() => {
+        calcScore();
+    }, [calcScore, rowsCleared, score])
+        return [score, setScore, rows, setRows, level, setLevel];
+}   
